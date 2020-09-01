e9398be8c8388a9e9b6fc3c42228db1c68f4ef2d
vagrant <vagrant@localhost.localdomain>
2020-09-01 07:16:41 +0000

    Finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,7 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
+    @books = Book.all
     if @book.save
       redirect_to book_path(@book), notice: "successfully created book!"
     else
@@ -19,10 +20,12 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
-    if @book.update()
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       redirect_to book_path(@book), notice: "successfully updated book!"
     else
       render 'edit'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   resources :books
+  root 'home#top'
+  get 'top' => 'home#top'
 end
